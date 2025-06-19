370af9298d5ae26848ec8934bc15a6a9da1c9224
MOHD JAMAL AFSAR <mohd.jama412@gmail.com>
2025-06-19 12:47:56 +0530

    Update backend.tf

--- ToDo-App/backend.tf
+++ ToDo-App/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "eksterrabucket"
+    bucket = "eks-terra-bucket-09849"
     key    = "backend/ToDo-App.tfstate"
     region = "us-east-1"
     dynamodb_table = "dynamoDB-terra"
