38370fa7e1c049b5a819b00cbb75687f3e2d398f
MOHD JAMAL AFSAR <mohd.jama412@gmail.com>
2025-06-19 15:07:26 +0530

    Update main.tf

--- ToDo-App/main.tf
+++ ToDo-App/main.tf
@@ -1,3 +1,40 @@
+resource "aws_s3_bucket" "enterprise_backend_state" {
+  bucket = "eks-terra-bucket-098498"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.enterprise_backend_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.enterprise_backend_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "eks-terra-bucket-098498"
+    key    = "backend/ToDo-App.tfstate"
+    region = "us-east-1"
+    encrypt        = true
+  #  dynamodb_table = "dynamoDB-terra"
+    use_lockfile = true
+  }
+}
+
+
 # create VPC
 module "VPC" {
   source           = "../modules/vpc"
