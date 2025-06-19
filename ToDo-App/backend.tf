8ae4773b6ce466fa1d9e646c2affd1001742fd9e
MOHD JAMAL AFSAR <mohd.jama412@gmail.com>
2025-06-19 13:02:12 +0530

    Update backend.tf

--- ToDo-App/backend.tf
+++ ToDo-App/backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket = "eks-terra-bucket-09849"
     key    = "backend/ToDo-App.tfstate"
     region = "us-east-1"
-    dynamodb_table = "dynamoDB-terra"
+  #  dynamodb_table = "dynamoDB-terra"
+    use_lockfile = true
   }
 }
