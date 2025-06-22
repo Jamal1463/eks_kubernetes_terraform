91dac4b980ea7eb173882d83e213faeac6310e69
MOHD JAMAL AFSAR <mohd.jama412@gmail.com>
2025-06-22 20:38:47 +0530

    update config files

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "vpc" {
   instance_tenancy = "default"
   enable_dns_hostnames = true
   enable_dns_support   = true
-  enable_classiclink_dns_support = false
+#  enable_classiclink_dns_support = false
   assign_generated_ipv6_cidr_block = false
 
 
